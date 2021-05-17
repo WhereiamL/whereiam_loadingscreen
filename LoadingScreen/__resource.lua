e2aca82addd43304fb80656f54e9f283ce6abbd4
WhereiamL <84282589+WhereiamL@users.noreply.github.com>
2021-05-17 18:15:23 +0200

    Add files via upload

---
+++ LoadingScreen/__resource.lua
@@ -0,0 +1,23 @@
+files {
+    'index.html',
+    'css/style.css',
+    'css/font.css',
+    'img/logo.png',
+    'music/loading1.ogg',
+    'font/Orbitron-Black',
+    'font/Orbitron-Black.woff2',
+    'font/Orbitron-Bold',
+    'font/Orbitron-Bold.woff2',
+    'font/Orbitron-ExtraBold',
+    'font/Orbitron-ExtraBold.woff2',
+    'font/Orbitron-Medium',
+    'font/Orbitron-Medium.woff2',
+    'font/Orbitron-Regular',
+    'font/Orbitron-Regular.woff2',
+    'font/Orbitron-SemiBold',
+    'font/Orbitron-SemiBold.woff2'
+}
+
+loadscreen 'index.html'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
